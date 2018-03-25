93c749ab46cf89f196abe8b1f03d9dfd15ff09f3
Salah Alsasa <salahalsasa@gmail.com>
2018-03-25 18:08:10 +0300

    update route file

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,24 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::prefix('cpanel')->middleware('auth')->group(function() {
 
-Route::get('cpanel/courses_categories', 'CourseCategoriesController@index');
+  Route::prefix('courses_categories')->group(function() {
+    Route::get('', 'CourseCategoriesController@index');
+    Route::get('add', 'CourseCategoriesController@add')->name('add_course_category');
+    Route::post('create', 'CourseCategoriesController@create')->name('create_course_category');
+    Route::get('edit/{id}', 'CourseCategoriesController@edit');
+    Route::post('update', 'CourseCategoriesController@update');
+    Route::post('delete', 'CourseCategoriesController@delete');
+  });
 
-Route::get('cpanel/courses_categories/add', 'CourseCategoriesController@add')->name('add_course_category');
-Route::post('cpanel/courses_categories/create', 'CourseCategoriesController@create')->name('create_course_category');
+  Route::prefix('courses')->group(function() {
+    Route::get('', 'CoursesController@index');
+    Route::get('add', 'CoursesController@add')->name('add_course');
+    Route::post('create', 'CoursesController@create')->name('create_course');
+    Route::get('edit/{id}', 'CoursesController@edit');
+    Route::post('update', 'CoursesController@update');
+    Route::post('delete', 'CoursesController@delete');
+  });
 
-
-Route::get('cpanel/courses_categories/edit/{id}', 'CourseCategoriesController@edit');
+});
